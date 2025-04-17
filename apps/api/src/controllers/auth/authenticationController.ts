6281fbbda8d73c0a476fb8a629b1445b83756c1e
bnzinxm <brenobarbosa269@gmail.com>
2025-04-17 18:56:36 -0300

    Controller in progress

---
+++ apps/api/src/controllers/auth/authenticationController.ts
@@ -0,0 +1,24 @@
+// src/controllers/marketAuthController.ts
+import { Request, Response } from 'express';
+import { AuthenticationService } from '../../services/auth/authenticationServices';
+
+const authService = new AuthenticationService();
+
+export const MarketAuthController = {
+  register: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+      const result = await authService.Markets.New({ email, password });
+
+      return res.status(201).json({
+        message: 'Mercado criado com sucesso.',
+        data: result,
+      });
+    } catch (err: any) {
+      return res.status(400).json({
+        error: err.message || 'Erro ao registrar mercado.',
+      });
+    }
+  },
+};
