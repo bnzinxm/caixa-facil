32f63c708c2db8fc43de2be98302a9abce7bbafc
bnzinxm <brenobarbosa269@gmail.com>
2025-04-18 12:03:51 -0300

    login route added

--- apps/api/src/controllers/auth/authenticationController.ts
+++ apps/api/src/controllers/auth/authenticationController.ts
@@ -6,10 +6,10 @@ const authService = new AuthenticationService();
 
 export const MarketAuthController = {
   register: async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+    const { nome, email, password } = req.body;
 
     try {
-      const result = await authService.Markets.New({ email, password });
+      const result = await authService.Markets.New({ nome, email, password });
 
       return res.status(201).json({
         message: 'Mercado criado com sucesso.',
@@ -19,6 +19,30 @@ export const MarketAuthController = {
         console.log(err);
       return res.status(400).json({
         error: err.message || 'Erro ao registrar mercado.',
+      });
+    }
+  },
+
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+      // autenticar o mercado
+      const result = await authService.Markets.Authenticate({ email, password });
+
+      // retornar sucesso com o token JWT
+      return res.status(200).json({
+        success: true,
+        message: '🛒 Mercado autenticado com sucesso!',
+        token: result.token, // o token retornado do serviço de autenticação
+        timestamp: new Date().toISOString(),
+      });
+    } catch (error: any) {
+      // retornar erro em caso de falha na autenticação
+      return res.status(400).json({
+        success: false,
+        message: error.message || 'Erro ao autenticar mercado.',
+        timestamp: new Date().toISOString(),
       });
     }
   },
